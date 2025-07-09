4b3e974741081e70bc7b6989e31583aae07dfda1
Dmitry Fomin <dfomin@highresbio.com>
2025-07-09 10:24:33 -0700

    plugins

---
+++ lua/d3man4ig/plugins/code-companion.lua
@@ -0,0 +1,12 @@
+return {
+  'olimorris/codecompanion.nvim',
+  opts = {
+    chat_split = {
+      width = 90,
+    },
+  },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+  },
+}
